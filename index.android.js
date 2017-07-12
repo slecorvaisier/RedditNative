690a1dceee8eb9500324939287122e5e124729ff
Steven Le Corvaisier <steven.lecorvaisier@gmail.com>
2017-07-12 22:43:17 +0200

    remove comments

--- index.android.js
+++ index.android.js
@@ -7,42 +7,4 @@ import {
 } from 'react-native';
 import AppContainer from './src';
 
-// export default class RedditNative extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Hey ! Welcome to React Nasddstive!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit index.android.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           Double tap R on your keyboard to reload,{'\n'}
-//           Shake or press menu button for dev menu
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
 AppRegistry.registerComponent('RedditNative', () => AppContainer);
